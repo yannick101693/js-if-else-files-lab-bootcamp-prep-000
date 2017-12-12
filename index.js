42cb81793851d25fa984f2987819cf63135900e2
yannick101693 <yannick101693@outlook.com>
2017-12-12 20:14:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ var name = "Susan"
 
 if (name === "Susan") {
     console.log("The name is correct")
-} 
+}
 else {
     console.log("Expected the name to be Susan")
 }
 // Don't worry about this
 module.exports = { name, height
 }
-
